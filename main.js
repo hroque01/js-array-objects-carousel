5918328526e393ad5f4711102242be9eeaad5886
hroque01 <hadson.roque01@gmail.com>
2022-11-22 18:16:20 +0100

    Button up infinite

---
+++ main.js
@@ -0,0 +1,75 @@
+
+// dichiaro l'array
+const images = [ 
+
+    {   image: 'img/01.webp', 
+        title: 'Marvel\'s Spiderman Miles Morale', 
+        text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.', 
+    }, 
+    {   image: 'img/02.webp', 
+        title: 'Ratchet & Clank: Rift Apart', 
+        text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.', 
+    },
+    { 
+        image: 'img/03.webp', 
+        title: 'Fortnite', 
+        text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.", 
+    }, 
+    { 
+        image: 'img/04.webp', 
+        title: 'Stray', 
+        text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city', 
+    },
+    { 
+        image: 'img/05.webp', 
+        title: "Marvel's Avengers", 
+        text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.', 
+    },
+
+];
+
+cicloImmagini()
+
+function cicloImmagini () {
+
+    for ( let i = 0; i < images.length; i++ ) {
+
+        let newCard = `
+        <div class="items">
+            <img src="${images[i].image}" alt="">
+            <h3 class="title">${images[i].title}</h3>
+            <p class="txt">${images[i].text}</p>
+        </div>
+        `
+        // questo e' il contenitore dei div che dovrebbero uscire
+        document.getElementById("items-blocks").innerHTML += newCard
+    }
+
+}
+
+let box = document.querySelector(".items").classList.add("active")
+
+let btnUp = document.querySelector(".button-up");
+
+let active = 0
+
+btnUp.addEventListener("click" , function(){
+
+    if (active === images.length - 1){
+        active = 0
+    } else {
+        active ++
+    }
+
+    document.querySelector(".items.active").classList.remove("active")
+    document.getElementsByClassName("items")[active].classList.add("active")
+});
+
+
+
+
+
+
+
+
+
